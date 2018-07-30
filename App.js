8797f77095c6dd7ff2e48d9b0cb5c0670f0eb6e6
TheAsouka <TA@theworld.home>
2018-07-30 12:32:39 +0200

    J'ai faim

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
 
-class Mercos extends Component{
+class Merco extends Component{
   render(){
     let img = {
       uri: 'https://www.motorlegend.com/images-voiture/hd/mercedes-classe-gla-45-amg-4matic-381-ch-113337.jpg'
     };
     return (
-      <Image source={img} style={{ width: 370, height: 200 }} />
+      <Image source={img} style={{ width: this.props.width, height: this.props.height }} />
+    );
+  }
+}
+
+class DescriptionVoiture extends Component{
+  render(){
+    return (
+      <Text style={{color: '#5342f4'}}>Ceci est un {this.props.name} !</Text>
     );
   }
 }
@@ -16,13 +24,16 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <Mercos/>
+        <Merco width={350} height={200}/>
+
         <Text>On sort le merco, vous êtes jaloux !</Text>
-      </View>      
+        <DescriptionVoiture name="GLA45 AMG 4Matic"/>
+
+        <Merco width={35} height={20}/>
+      </View>     
     );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
